5613125ac51086dcde381569998738edaa6fc87b
ivolocastic <ivo@locastic.com>
2023-11-16 08:23:45 +0100

    finished canister

---
+++ src/index.old.ts
@@ -0,0 +1,177 @@
+import {
+    Record,
+    StableBTreeMap,
+    Vec, Result,
+    nat64,
+    ic, float64
+} from 'azle';
+import { v4 as uuidv4 } from 'uuid';
+
+type Bank = Record<{
+  totalDeposit: float64;
+  transactions: Vec<BankTransaction>;
+}>;
+
+type BankTransaction = Record<{
+  id: string;
+  amount: float64;
+  timestamp: nat64;
+  customerID: string;
+  operation: string;
+}>;
+
+type BankTransactionData = Record<{
+  amount: float64;
+  operation: string;
+}>;
+
+type Customer = Record<{
+  id: string;
+  username: string;
+  password: string;
+  amount: float64;
+}>;
+
+type CustomerData = Record<{
+  username: string;
+  password: string;
+  amount: float64;
+}>;
+
+const bankStorage = new StableBTreeMap<string, Bank>(0, 44, 2048);
+const customerStorage = new StableBTreeMap<string, Customer>(1, 44, 1024);
+const transactionStorage = new StableBTreeMap<string, BankTransaction>(
+  2,
+  44,
+  2048
+);
+let currentCustomer: Customer | null = null;
+
+//BANK
+
+$query;
+export function getBankDetails(): Result<Bank, string> {
+  return Result.Ok<Bank, string>(bankStorage.values()[0]);
+}
+
+$query;
+export function getBalance(): Result<string, string> {
+  if (!currentCustomer) {
+    return Result.Err<string, string>('There is no logged in customer.');
+  }
+  return Result.Ok<string, string>(
+    `Your balance is: ${currentCustomer.amount} $`
+  );
+}
+
+//CUSTOMER
+$update;
+export function createCustomer(
+  data: CustomerData
+): Result<string, string> {
+  const customer = customerStorage
+    .values()
+    .filter((c) => c.username === data.username)[0];
+  if (customer) {
+    return Result.Err<string, string>('Customer already exists');
+  }
+
+  const newCustomer = {
+    id: uuidv4(),
+    ...data,
+  };
+  customerStorage.insert(newCustomer.id, newCustomer);
+  return Result.Ok<string, string>(
+    `Customer ${newCustomer.username} added successfully.`
+  );
+}
+
+$update;
+export function authenticateCustomer(
+  username: string,
+  password: string
+): Result<string, string> {
+  const customer = customerStorage
+    .values()
+    .filter((c) => c.username === username)[0];
+  if (!customer) {
+    return Result.Err<string, string>(`Customer ${username} does not exist.`);
+  }
+  if (customer.password !== password) {
+    return Result.Err<string, string>('Credentials not matching.');
+  }
+  currentCustomer = customer;
+  return Result.Ok<string, string>('Logged in.');
+}
+
+export function signOut(): Result<string, string> {
+  if (!currentCustomer) {
+    return Result.Err<string, string>('There is no logged in customer.');
+  }
+  currentCustomer = null;
+  return Result.Ok<string, string>('Logged out.');
+}
+
+//TRANSACTIONS
+
+$update;
+export function createTransaction(
+  data: BankTransactionData
+): Result<string, string> {
+  if (!currentCustomer) {
+    return Result.Err<string, string>(
+      'Only logged in customer can perform this operation.'
+    );
+  }
+  if (data.operation === 'deposit') {
+    currentCustomer.amount += data.amount;
+  } else {
+    if (currentCustomer.amount < data.amount) {
+      return Result.Err<string, string>(
+        'Account balance lower than withdrawal amount.'
+      );
+    }
+    currentCustomer.amount -= data.amount;
+  }
+
+  const newTransaction: BankTransaction = {
+    id: uuidv4(),
+    timestamp: ic.time(),
+    amount: data.amount,
+    operation: data.operation,
+    customerID: currentCustomer.id,
+  };
+  transactionStorage.insert(currentCustomer.id, newTransaction);
+  return Result.Ok<string, string>('Operation completed successfully.');
+}
+
+$query;
+export function getCustomerTransactions(): Result<
+  Vec<BankTransaction>,
+  string
+> {
+  if (!currentCustomer) {
+    return Result.Err<Vec<BankTransaction>, string>(
+      'Only logged in customer can perform this operation.'
+    );
+  }
+  const customerTransactions = transactionStorage
+    .values()
+    .filter((transaction) => transaction.customerID === currentCustomer?.id);
+
+  return Result.Ok<Vec<BankTransaction>, string>(customerTransactions);
+}
+
+// a workaround to make uuid package work with Azle
+globalThis.crypto = {
+  // @ts-ignore
+  getRandomValues: () => {
+    let array = new Uint8Array(32);
+
+    for (let i = 0; i < array.length; i++) {
+      array[i] = Math.floor(Math.random() * 256);
+    }
+
+    return array;
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,161 +1,168 @@
 import {
-    $query,
-    $update,
-    Record,
-    StableBTreeMap,
-    Vec, Result,
-    nat64,
-    ic, float64
+  Canister,
+  query,
+  text,
+  update,
+  Void,
+  Record,
+  float64,
+  Vec,
+  nat64,
+  StableBTreeMap,
+  nat8,
+  Principal,
+  Err,
+  Ok,
+  Result,
+  ic,
 } from 'azle';
-import { v4 as uuidv4 } from 'uuid';
 
-type Bank = Record<{
-  totalDeposit: float64;
-  transactions: Vec<BankTransaction>;
-}>;
+const BankTransaction = Record({
+  id: Principal,
+  amount: float64,
+  timestamp: nat64,
+  customerID: Principal,
+  operation: text,
+});
 
-type BankTransaction = Record<{
-  id: string;
-  amount: float64;
-  timestamp: nat64;
-  customerID: string;
-  operation: 'deposit' | 'withdraw';
-}>;
+const Customer = Record({
+  id: Principal,
+  username: text,
+  password: text,
+  amount: float64,
+});
 
-type BankTransactionData = Record<{
-  amount: float64;
-  operation: 'deposit' | 'withdraw';
-}>;
+const Bank = Record({
+  totalDeposit: float64,
+  transactions: Vec(BankTransaction),
+  customers: Vec(Customer),
+});
 
-type Customer = Record<{
-  id: string;
-  username: string;
-  password: string;
-  amount: float64;
-}>;
+const bankStorage: typeof Bank = {
+  totalDeposit: 0,
+  transactions: [],
+  customers: [],
+}; //make stable to persist state on updates
+const customerStorage = StableBTreeMap(Principal, Customer, 1);
+const transactionStorage = StableBTreeMap(Principal, BankTransaction, 2);
+let currentCustomer: typeof Customer | null; 
 
-const bankStorage = new StableBTreeMap<string, Bank>(0, 44, 2048);
-const customerStorage = new StableBTreeMap<string, Customer>(1, 44, 1024);
-const transactionStorage = new StableBTreeMap<string, BankTransaction>(
-  2,
-  44,
-  2048
-);
-let currentCustomer: Customer | null = null;
-
-//BANK
-
-$query;
-export function getBankDetails(): Result<Bank, string> {
-  return Result.Ok<Bank, string>(bankStorage.values()[0]);
-}
-
-$query;
-export function getBalance(): Result<string, string> {
-  if (!currentCustomer) {
-    return Result.Err<string, string>('There is no logged in customer.');
-  }
-  return Result.Ok<string, string>(
-    `Your balance is: ${currentCustomer.amount} $`
-  );
-}
-
-//CUSTOMER
-$update;
-export function createCustomer(
-  data: Omit<Customer, 'id'>
-): Result<string, string> {
-  const customer = customerStorage
-    .values()
-    .filter((c) => c.username === data.username)[0];
-  if (customer) {
-    return Result.Err<string, string>('Customer already exists');
-  }
-
-  const newCustomer = {
-    id: uuidv4(),
-    ...data,
-  };
-  customerStorage.insert(newCustomer.id, newCustomer);
-  return Result.Ok<string, string>(
-    `Customer ${newCustomer.username} added successfully.`
-  );
-}
-
-$update;
-export function authenticateCustomer(
-  username: string,
-  password: string
-): Result<string, string> {
-  const customer = customerStorage
-    .values()
-    .filter((c) => c.username === username)[0];
-  if (!customer) {
-    return Result.Err<string, string>(`Customer ${username} does not exist.`);
-  }
-  if (customer.password !== password) {
-    return Result.Err<string, string>('Credentials not matching.');
-  }
-  currentCustomer = customer;
-  return Result.Ok<string, string>('Logged in.');
-}
-
-export function signOut(): Result<string, string> {
-  if (!currentCustomer) {
-    return Result.Err<string, string>('There is no logged in customer.');
-  }
-  currentCustomer = null;
-  return Result.Ok<string, string>('Logged out.');
-}
-
-//TRANSACTIONS
-
-$update;
-export function createTransaction(
-  data: BankTransactionData
-): Result<string, string> {
-  if (!currentCustomer) {
-    return Result.Err<string, string>(
-      'Only logged in customer can perform this operation.'
-    );
-  }
-  if (data.operation === 'deposit') {
-    currentCustomer.amount += data.amount;
-  } else {
-    if (currentCustomer.amount < data.amount) {
-      return Result.Err<string, string>(
-        'Account balance lower than withdrawal amount.'
-      );
+export default Canister({
+  getBankDetails: query([], Result(Bank, text), () => {
+    return Ok(bankStorage);
+  }),
+  getBalance: query([], Result(text, text), () => {
+    if (!currentCustomer) {
+      return Err('There is no logged in customer');
     }
-    currentCustomer.amount -= data.amount;
-  }
+    return Ok(`Your balance is ${currentCustomer.amount}`);
+  }),
 
-  const newTransaction: BankTransaction = {
-    id: uuidv4(),
-    timestamp: ic.time(),
-    amount: data.amount,
-    operation: data.operation,
-    customerID: currentCustomer.id,
-  };
-  transactionStorage.insert(currentCustomer.id, newTransaction);
-  return Result.Ok<string, string>('Operation completed successfully.');
-}
-
-$query;
-export function getCustomerTransactions(): Result<
-  Vec<BankTransaction>,
-  string
-> {
-  if (!currentCustomer) {
-    return Result.Err<Vec<BankTransaction>, string>(
-      'Only logged in customer can perform this operation.'
+  //TRANSACTIONS
+  getCustomerTransactions: query([], Result(Vec(BankTransaction), text), () => {
+    if (!currentCustomer) {
+      return Err('Only logged in customer can perform this operation.');
+    }
+    const transactions = transactionStorage.values();
+    const customerTransactions = transactions.filter(
+      (transaction: typeof BankTransaction) =>
+        transaction.customerID === currentCustomer.id
     );
-  }
-  const customerTransactions = transactionStorage
-    .values()
-    .filter((transaction) => transaction.customerID === currentCustomer?.id);
+    return Ok(customerTransactions);
+  }),
+  createTransaction: update(
+    [float64, text],
+    Result(text, text),
+    (amount, operation) => {
+      if (!currentCustomer) {
+        return Err('Only logged in customer can perform this operation.');
+      }
 
-  return Result.Ok<Vec<BankTransaction>, string>(customerTransactions);
+      if (operation === 'deposit') {
+        currentCustomer.amount += amount;
+        bankStorage.totalDeposit += amount;
+      } else {
+        if (currentCustomer.amount < amount) {
+          return Err('Account balance lower than withdrawal amount.');
+        }
+        currentCustomer.amount -= amount;
+        bankStorage.totalDeposit -= amount;
+      }
+      const newTransaction: typeof BankTransaction = {
+        id: generateId(),
+        amount,
+        operation,
+        timestamp: ic.time(),
+        customerID: currentCustomer.id,
+      };
+      transactionStorage.insert(newTransaction.id, newTransaction);
+      customerStorage.insert(currentCustomer.id, { ...currentCustomer });
+      return Ok('Transaction successful.');
+    }
+  ),
+
+  //CUSTOMER
+  createCustomer: update(
+    [text, text, float64],
+    Result(text, text),
+    (username, password, amount) => {
+      const customer = customerStorage
+        .values()
+        .filter((c: typeof Customer) => c.username === username)[0];
+      if (customer) {
+        return Err('Customer already exists.');
+      }
+      const newCustomer: typeof Customer = {
+        id: generateId(),
+        username,
+        password,
+        amount,
+      };
+      customerStorage.insert(newCustomer.id, newCustomer);
+      bankStorage.totalDeposit += newCustomer.amount;
+      return Ok(`Customer ${newCustomer.username} added successfully.`);
+    }
+  ),
+
+  authenticateCustomer: update(
+    [text, text],
+    Result(text, text),
+    (username, password) => {
+      const customer = customerStorage
+        .values()
+        .filter((c: typeof Customer) => c.username === username)[0];
+      if (!customer) {
+        return Err('Customer does not exist.');
+      }
+      if (customer.password !== password) {
+        return Err('Customer with provided credentials does not exist.');
+      }
+      currentCustomer = customer;
+      return Ok('Logged in');
+    }
+  ),
+  signOut: update([], Result(text, text), () => {
+    if (!currentCustomer) {
+      return Err('There is no logged in customer.');
+    }
+    currentCustomer = null;
+    return Ok('Logged out.');
+  }),
+
+  getAuthenticatedCustomer: query([], Result(text, text), () => {
+    if (!currentCustomer) {
+      return Err('There is no logged in customer.');
+    }
+    return Ok(currentCustomer.username);
+  }),
+});
+
+function generateId(): Principal {
+  const randomBytes = new Array(29)
+    .fill(0)
+    .map((_) => Math.floor(Math.random() * 256));
+
+  return Principal.fromUint8Array(Uint8Array.from(randomBytes));
 }
 
 // a workaround to make uuid package work with Azle
